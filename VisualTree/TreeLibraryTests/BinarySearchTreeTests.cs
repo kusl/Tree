1096b1933a17ed7ee3f190754299e6fb148a9dd1
kushal <kushaldeveloper@gmail.com>
2016-05-06 17:08:48 -0400

    create tests

---
+++ VisualTree/TreeLibraryTests/BinarySearchTreeTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TreeLibraryTests
+{
+    [TestClass]
+    public class BinarySearchTreeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
