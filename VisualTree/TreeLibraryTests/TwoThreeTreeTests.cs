2d5e300771358d849fafa866c5452cee002d26d7
kushal <kushaldeveloper@gmail.com>
2016-05-19 15:12:25 -0400

    create test for instantiating two three tree
    yes, it looks weird
    I'll look into it more

---
+++ VisualTree/TreeLibraryTests/TwoThreeTreeTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TreeLibrary;
+
+namespace TreeLibraryTests
+{
+    [TestClass]
+    public class TwoThreeTreeTests
+    {
+        [TestMethod]
+        public void InstantiateTwoThreeTree()
+        {
+            TwoThreeTree tree = new TwoThreeTree(5);
+            Assert.AreEqual(-1, tree.Top);
+        }
+    }
+}
