f66d6b483c41c31a1b3413e8f419ec64aad69262
kushal <kushaldeveloper@gmail.com>
2016-05-21 18:11:46 -0400

    create 2-3 tree test node

---
+++ VisualTree/TreeLibraryTests/TwoThreeNodeTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TreeLibrary;
+
+namespace TreeLibraryTests
+{
+    [TestClass]
+    public class TwoThreeNodeTests
+    {
+        [TestMethod]
+        public void InstantiateTwoThreeNode()
+        {
+            TwoThreeNode node = new TwoThreeNode();
+            Assert.IsNull(node.LeftData);
+            Assert.IsNull(node.RightData);
+            Assert.AreEqual(0, node.LeftChild);
+            Assert.AreEqual(0, node.MiddleChild);
+            Assert.AreEqual(0, node.RightChild);
+            Assert.AreEqual(0, node.LeftDataDuplicates);
+            Assert.AreEqual(0, node.RightDataDuplicates);
+        }
+    }
+}
