656b889564bcacf37d9ce454ba854c11849610ff
kushal <kushaldeveloper@gmail.com>
2016-05-23 19:16:08 -0400

    add red black tree
    from http://www.codeproject.com/Articles/8287/Red-Black-Trees-in-C

--- VisualTree/TreeLibraryTests/TwoThreeTreeTests.cs
+++ VisualTree/TreeLibraryTests/TwoThreeTreeTests.cs
@@ -13,5 +13,28 @@ namespace TreeLibraryTests
             TwoThreeTree tree = new TwoThreeTree(5);
             Assert.AreEqual(-1, tree.Top);
         }
+        [TestMethod]
+        public void TestInsertRoot()
+        {
+            TwoThreeTree tree = new TwoThreeTree(5);
+            tree.InsertTwoThree("apple");
+            Assert.AreEqual(1, tree.FindNode("cat"));
+            Assert.AreEqual(1, tree.FindNode("dog"));
+            Assert.AreEqual(1, tree.FindNode("egg"));
+            Assert.AreEqual(-1, tree.FindNode("apple"));
+        }
+        [TestMethod]
+        public void TestInsertMoreNodes()
+        {
+            TwoThreeTree tree = new TwoThreeTree(100000);
+            for(int i = 0; i < 100; i++)
+            {
+                tree.InsertTwoThree(i.ToString());
+            }
+            for(int i = 100; i < 200; i++)
+            {
+                Assert.IsTrue(tree.FindNode(i.ToString()) > 0);
+            }
+        }
     }
 }
