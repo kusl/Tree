fedca50a562df7b6eb38c468dd86340f236d3026
kushal <kushaldeveloper@gmail.com>
2016-05-23 20:18:44 -0400

    remove red black project

---
+++ VisualTree/TreeLibraryTests/RedBlackTreeTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TreeLibrary;
+
+namespace TreeLibraryTests
+{
+    [TestClass]
+    public class RedBlackTreeTests
+    {
+        [TestMethod]
+        public void InstantiateRedBlackTree()
+        {
+            RedBlackTree tree = new RedBlackTree("apple");
+            int myKey = 1;
+            string myName = "Tony";
+            tree.Add(myKey, myName);
+            Assert.AreEqual(1, tree.Size());
+        }
+    }
+}
