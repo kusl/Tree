5c149a4ada5f1a4e23ead98560605bba61540037
kushal <kushaldeveloper@gmail.com>
2016-05-24 08:04:51 -0400

    use TreeLibrary in TreeConsole

--- VisualTree/TreeConsole/Program.cs
+++ VisualTree/TreeConsole/Program.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TreeLibrary;
 
 namespace TreeConsole
 {
     class Program
     {
+        static readonly int numberOfNumbers = 100000;
         static void Main(string[] args)
         {
+            int[] inputArray = new int[numberOfNumbers];
+            BinarySearchTree bst = new BinarySearchTree();
+            AvlTree<int, int> avlTree = new AvlTree<int, int>();
+            RedBlackTree redBlackTree = new RedBlackTree("rbTree");
+            TwoThreeTree twoThreeTree = new TwoThreeTree(numberOfNumbers * numberOfNumbers);
+            foreach(int i in inputArray)
+            {
+                bst.Add(i);
+                avlTree.Insert(i, i);
+                redBlackTree.Add(i, i);
+                twoThreeTree.InsertTwoThree(i.ToString());
+            }
+        }
+        static int[] GenerateNumbers()
+        {
+            int[] result = new int[numberOfNumbers];
+            for(int i = 0; i < numberOfNumbers; i++)
+            {
+                Random myRandom = new Random();
+                result[i] = myRandom.Next(1, numberOfNumbers);
+            }
+            return result;
         }
     }
 }
