d5aa352fb8526dacb7d956ee614bb6ec5decc6d8
kushal <kushaldeveloper@gmail.com>
2016-05-24 17:50:28 -0400

    keep score

---
+++ VisualTree/TreeConsole/MyOutput.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeConsole
+{
+    class MyOutput
+    {
+        public int Comparisons { get; set; }
+    }
+    class MyClass
+    {
+        public int Ceiling { get; set; }
+        public int Query { get; set; }
+        public MyOutput AVLTree { get; set; }
+        public MyOutput RBTree { get; set; }
+        public MyOutput TwoThreeTree { get; set; }
+        public override string ToString()
+        {
+            return String.Format("Ceiling: {0}, Query: {1}, AVLTree: {2}, RBTree: {3}, TwoThreeTree: {4}", Ceiling, Query, AVLTree.Comparisons, RBTree.Comparisons, TwoThreeTree.Comparisons);
+        }
+    }
+}
--- VisualTree/TreeConsole/Program.cs
+++ VisualTree/TreeConsole/Program.cs
@@ -9,17 +9,44 @@ namespace TreeConsole
 {
     class Program
     {
-        static readonly int max = 10000;
+        public static List<MyClass> myClasses { get; set; }
+        static readonly int max = 1000000;
         static void Main(string[] args)
         {
-
-            for (int numberOfNumbers = 100; numberOfNumbers < max; numberOfNumbers++)
+            myClasses = new List<MyClass>();
+            //for (int numberOfNumbers = 100; numberOfNumbers < max; numberOfNumbers++)
+            //{
+            //    if (numberOfNumbers % 1000 == 0)
+            //    {
+            //        int whileCounter = 0;
+            //        while (whileCounter < numberOfNumbers / 100)
+            //        {
+            //            RunTrees(numberOfNumbers); 
+            //        }
+            //    }
+            //}
+            for (int i = 0; i < 1000; i++)
             {
-                if (numberOfNumbers % 10 == 0)
+                RunTrees(max);
+            }
+            int avlScore = 0; int rbScore = 0; int ttScore = 0;
+            foreach (var myClass in myClasses)
+            {
+                int myMax = MoreMath.Min(myClass.AVLTree.Comparisons, myClass.RBTree.Comparisons, myClass.TwoThreeTree.Comparisons);
+                if (myMax == myClass.AVLTree.Comparisons)
                 {
-                    RunTrees(numberOfNumbers);
+                    avlScore++;
+                }
+                if (myMax == myClass.RBTree.Comparisons)
+                {
+                    rbScore++;
+                }
+                if (myMax == myClass.TwoThreeTree.Comparisons)
+                {
+                    ttScore++;
                 }
             }
+            WriteToTextFile(string.Format("{0}\t{1}\t{2}", avlScore, rbScore, ttScore));
         }
 
         private static void RunTrees(int numberOfNumbers)
@@ -36,13 +63,28 @@ namespace TreeConsole
             Random myRandom = new Random();
             int query = myRandom.Next(1, numberOfNumbers);
             int avlTreeOutParameter = 0;
+            MyClass myClass = new MyClass()
+            {
+                Ceiling = numberOfNumbers,
+                Query = query
+            };
             avlTree.Search(query, out avlTreeOutParameter);
-            WriteToTextFile(string.Format("AVL,{0},{1},{2}", StepCounter.ComparisonStep, numberOfNumbers, query));
+            myClass.AVLTree = new MyOutput()
+            {
+                Comparisons = StepCounter.ComparisonStep
+            };
             redBlackTree.Contains(query);
-            WriteToTextFile(string.Format("RBT,{0},{1},{2}", StepCounter.ComparisonStep, numberOfNumbers, query));
+            myClass.RBTree = new MyOutput()
+            {
+                Comparisons = StepCounter.ComparisonStep
+            };
             twoThreeTree.FindNode(query.ToString());
-            WriteToTextFile(string.Format("TTT,{0},{1},{2}", StepCounter.ComparisonStep, numberOfNumbers, query));
-            Console.WriteLine();
+            myClass.TwoThreeTree = new MyOutput()
+            {
+                Comparisons = StepCounter.ComparisonStep
+            };
+            WriteToTextFile(myClass.ToString());
+            myClasses.Add(myClass);
         }
 
         static void WriteToTextFile(string input)
@@ -52,6 +94,46 @@ namespace TreeConsole
             {
                 file.WriteLine(input);
             }
+        }
+    }
+    public static class MoreMath
+    {
+        // This method only exists for consistency, so you can *always* call
+        // MoreMath.Max instead of alternating between MoreMath.Max and Math.Max
+        // depending on your argument count.
+        public static int Max(int x, int y)
+        {
+            return Math.Max(x, y);
+        }
+
+        public static int Min(int x, int y)
+        {
+            return Math.Min(x, y);
+        }
+
+        public static int Max(int x, int y, int z)
+        {
+            // Or inline it as x < y ? (y < z ? z : y) : (x < z ? z : x);
+            // Time it before micro-optimizing though!
+            return Math.Max(x, Math.Max(y, z));
+        }
+
+        public static int Min(int x, int y, int z)
+        {
+            // Or inline it as x < y ? (y < z ? z : y) : (x < z ? z : x);
+            // Time it before micro-optimizing though!
+            return Math.Min(x, Math.Min(y, z));
+        }
+
+
+        public static int Max(int w, int x, int y, int z)
+        {
+            return Math.Max(w, Math.Max(x, Math.Max(y, z)));
+        }
+
+        public static int Max(params int[] values)
+        {
+            return Enumerable.Max(values);
         }
     }
 }
